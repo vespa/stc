c985e561153b878494bb21f4f0935e10fdde6647
Danilo Vespa <d.vespa@gmail.com>
2018-02-16 16:21:55 -0200

    added basic structure for filters

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import CSS from '../css/main.scss'
+import FilterSelect from "components/FilterSelect";
 import 'babel-polyfill'
 
 const wrapper = document.getElementById("root");
 
 const app = ()=>{
-	const itemClass = "app__item";
-	return 	<div className="app">
-				<header className="app__header">TraficMeister</header>
-				<div className="app__filters">
-					filters
-				</div>
-				<div className="app_results">
-					results
-				</div>
-			</div>
+    const itemClass = "app__item";
+    return  <div className="app">
+                <header className="app__header">TraficMeister</header>
+                <div className="app__filters">
+                    <div className="app__filters__list">
+                        <FilterSelect filter="FILTER_VEHICLE" />
+                        <FilterSelect filter="FILTER_VEHICLE_BRAND" />
+                        <FilterSelect filter="FILTER_VEHICLE_COLOR" />
+                    </div>
+                </div>
+                <div className="app_results">
+                    results
+                </div>
+            </div>
 }
 wrapper ? ReactDOM.render(app(), wrapper) : false;
---
+++ src/js/components/FilterSelect.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const FilterSelect = ({filter}) => {
+  return (
+    <select>
+      <option> teste </option>
+    </select>
+  )
+}
+FilterSelect.propTypes = {
+  filter: PropTypes.string.isRequired
+}
+
+export default FilterSelect
